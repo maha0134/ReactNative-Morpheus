a58c3fc432039e35c611116e3d152d9df6142dc7
Akshay Mahajan <maha0134@algonquinlive.com>
2022-12-14 20:49:39 -0500

    helper function restored

--- helpers/helperFunctions.js
+++ helpers/helperFunctions.js
@@ -1,6 +1,7 @@
 export function getDate() {
-  // let date = new Date(Date.now());
-  let date = new Date("12/13/2022");
+  let date = new Date(Date.now());
+  //To check, you can add any date under here.
+  // let date = new Date("12/13/2022");
   const time = date.getHours();
   date = date.toLocaleString("en-CA").split(time)[0];
   return date;
