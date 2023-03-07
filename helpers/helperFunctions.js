7f92b9fd11e0f93b424feab18f1f3a5f4132279e
Akshay Mahajan <maha0134@algonquinlive.com>
2023-03-07 09:04:40 -0500

    fix displayed date issue

--- helpers/helperFunctions.js
+++ helpers/helperFunctions.js
@@ -2,7 +2,7 @@ export function getDate() {
   let date = new Date(Date.now());
   //To check, you can add any date under here.
   // let date = new Date("12/13/2022");
-  const time = date.getHours();
+  const time = date.getHours().toString().padStart(2, "0");
   date = date.toLocaleString("en-CA").split(time)[0].trim();
   return date;
 }
